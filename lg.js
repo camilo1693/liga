0d7b1b88b07714f3b98d60ec67053a9c84628fd5
Camilo Aguilar <jkamilo93@hotmail.com>
2021-07-29 20:56:47 +0200

    con stylos

--- lg.js
+++ lg.js
@@ -1,26 +1,32 @@
 creartabla(partidos.matches);
-
-function creartabla() {
+function creartabla(juegos) {
   let cuerpotabla = document.getElementById("tb1");
-  for (let i = 0; i < partidos.matches.length; i++) {
+  for (let i = 0; i < juegos.length; i++) {
     const fila = document.createElement("tr");
 
     let local = document.createElement("p");
-    local.innerHTML = partidos.matches[i].homeTeam.name;
+    local.innerHTML = juegos[i].homeTeam.name;
     
+    let imagelocal = document.createElement("img");
+    imagelocal.src = "https://crests.football-data.org/"+ juegos[i].homeTeam.id + ".svg";
+    imagelocal.classList.add("escu");
 
     let visitante = document.createElement("p");
-    visitante.innerHTML = partidos.matches[i].awayTeam.name;
+    visitante.innerHTML = juegos[i].awayTeam.name;
+
+    let imagevisitante = document.createElement("img");
+    imagevisitante.src = "https://crests.football-data.org/"+ juegos[i].awayTeam.id + ".svg";
+    imagevisitante.classList.add("escu");
+
+    let fecha = new Date(juegos[i].utcDate);
     
-    let fecha = new Date(partidos.matches[i].utcDate);
     
-    
-    let jornada = partidos.matches[i].matchday;
+    let jornada = juegos[i].matchday;
     
     let resultado = document.createElement("p");
-    resultado.innerHTML = partidos.matches[i].score.fullTime.homeTeam + "-" + partidos.matches[i].score.fullTime.awayTeam;
+    resultado.innerHTML = juegos[i].score.fullTime.homeTeam + "-" + partidos.matches[i].score.fullTime.awayTeam;
 
-    let resultadopartido = [local, visitante, fecha.toLocaleString(), jornada, resultado];
+    let resultadopartido = [local, imagelocal, visitante, imagevisitante, fecha.toLocaleString(), jornada, resultado];
     for (let j = 0; j < resultadopartido.length ; j++) {
         const columna = document.createElement("td");
         columna.append(resultadopartido[j]);
--- tabalaliga.js
+++ tabalaliga.js
@@ -1,40 +1,40 @@
 creartabla(tab1.standings[0].table);
 
-function creartabla() {
+function creartabla(juegos1) {
   let cuerpotabla = document.getElementById("tb2");
-  for (let i = 0; i < tab1.standings[0].table.length; i++) {
+  for (let i = 0; i < juegos1.length; i++) {
     const fila = document.createElement("tr");
 
     let name = document.createElement("p");
-    name.innerHTML = tab1.standings[0].table[i].team.name;
+    name.innerHTML = juegos1[i].team.name;
 
     let image = document.createElement("img");
-    image.src = tab1.standings[0].table[i].team.crestUrl;
+    image.src = juegos1[i].team.crestUrl;
     image.classList.add("escu");
 
     let par = document.createElement("p");
-    par.innerHTML = tab1.standings[0].table[i].playedGames;
+    par.innerHTML = juegos1[i].playedGames;
 
     let v = document.createElement("p");
-    v.innerHTML = tab1.standings[0].table[i].won;
+    v.innerHTML = juegos1[i].won;
     
     let e = document.createElement("p");
-    e.innerHTML = tab1.standings[0].table[i].draw;
+    e.innerHTML = juegos1[i].draw;
 
     let lost = document.createElement("p");
-    lost.innerHTML = tab1.standings[0].table[i].lost;
+    lost.innerHTML = juegos1[i].lost;
 
     let gf = document.createElement("p");
-    gf.innerHTML = tab1.standings[0].table[i].goalsFor;
+    gf.innerHTML = juegos1[i].goalsFor;
 
     let gc = document.createElement("p");
-    gc.innerHTML = tab1.standings[0].table[i].goalsAgainst;
+    gc.innerHTML = juegos1[i].goalsAgainst;
 
     let dg = document.createElement("p");
-    dg.innerHTML = tab1.standings[0].table[i].goalDifference;
+    dg.innerHTML = juegos1[i].goalDifference;
 
     let pts = document.createElement("p");
-    pts.innerHTML = tab1.standings[0].table[i].points;
+    pts.innerHTML = juegos1[i].points;
 
     let resultadosclasi = [name, image, par, v, e, lost, gf, gc, dg, pts];
     for (let j = 0; j < resultadosclasi.length ; j++) {
